e96446da86a5ceda76aef0da2f9d0e00baa42bf4
Tacyniak <boris.tacyniak@viacesi.fr>
2014-11-28 10:39:38 +0100

    Fix license

--- src/Black/Component/User/UserEvents.php
+++ src/Black/Component/User/UserEvents.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * This file is part of the ${FILE_HEADER_PACKAGE} package.
+ * This file is part of the Black package.
  *
- * ${FILE_HEADER_COPYRIGHT}
+ * (c) Alexandre Balmes <alexandre@lablackroom.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,8 +12,8 @@ namespace Black\Component\User;
 /**
  * Class UserEvents
  *
- * @author Alexandre Balmes <${COPYRIGHT_NAME}>
- * @license ${COPYRIGHT_LICENCE}
+ * @author  Alexandre 'pocky' Balmes <alexandre@lablackroom.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
  */
 final class UserEvents
 {
